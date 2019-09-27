3bed6e9bec8be0b798f2eb890a1fab6277737cd8
Artemios <demisbalbach@googlemail.com>
2019-09-27 17:21:47 +0200

    Update init.el

--- init.el
+++ init.el
@@ -5,10 +5,11 @@
 (setq package-enable-at-startup nil)
 
 ;;; remove SC if you are not using sunrise commander and org if you like outdated packages
-(setq package-archives '(("ELPA"  . "http://tromey.com/elpa/")
-			 ("gnu"   . "http://elpa.gnu.org/packages/")
+(setq package-archives '(("ELPA"  . "https://tromey.com/elpa/")
+			 ("gnu"   . "https://elpa.gnu.org/packages/")
 			 ("melpa" . "https://melpa.org/packages/")
 			 ("org"   . "https://orgmode.org/elpa/")))
+(setq package-check-signature nil)
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
