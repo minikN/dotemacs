2b5b72e7a8e37288a03e3192ca0a8fefa0eb97fd
Artemios <demisbalbach@googlemail.com>
2019-09-27 12:11:04 +0200

    Create init.el

---
+++ init.el
@@ -0,0 +1,9 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+  '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
