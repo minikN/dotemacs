2fe7f969ad3790fde069d9346c090f3f70c00e28
Artemios <demisbalbach@googlemail.com>
2019-09-27 12:33:10 +0200

    Update init.el

--- init.el
+++ init.el
@@ -1,11 +1,19 @@
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6)
+
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-  '("melpa" . "https://melpa.org/packages/"))
+
+;;; remove SC if you are not using sunrise commander and org if you like outdated packages
+(setq package-archives '(("ELPA"  . "http://tromey.com/elpa/")
+			 ("gnu"   . "http://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")
+			 ("org"   . "https://orgmode.org/elpa/")))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(when (file-readable-p "~/.emacs.d/config.org")
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
