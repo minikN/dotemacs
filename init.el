8202fb859ad35bd6f40c8594219d8aa54bf67be7
Artemios <demisbalbach@googlemail.com>
2019-09-27 12:27:56 +0200

    Update init.el

--- init.el
+++ init.el
@@ -7,3 +7,9 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
+(unless (package-installed-p 'monokai-pro-theme)
+  (package-refresh-contents)
+  (package-install 'monokai-pro-theme))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
