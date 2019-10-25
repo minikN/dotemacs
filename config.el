1042957fdc0a28b34c7d658bf41da734465e7ab8
U-CPT\deb <deb@DEMBAL-W10-LAP.campoint.net>
2019-10-25 08:43:16 +0200

    changed some stuff

--- config.el
+++ config.el
@@ -2,20 +2,70 @@
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-(use-package monokai-pro-theme
-  :ensure t
-  :config
-  (load-theme 'monokai-pro t))
+(unless (package-installed-p 'spacemacs-theme)
+  (package-refresh-contents)
+  (package-install 'spacemacs-theme))
+
+(custom-set-variables '(spacemacs-theme-custom-colors
+			'((base . "#00ff00"))))
 
 (use-package which-key
   :ensure t
   :init
   (which-key-mode))
 
+(use-package company
+  :ensure t
+  :init
+  (add-hook 'after-init-hook 'global-company-mode))
+
 (use-package avy
   :ensure t
   :bind
   ("M-s" . avy-goto-char))
+
+(use-package hungry-delete
+  :ensure t
+  :config (global-hungry-delete-mode))
+
+(if (eq system-type 'gnu/linux)
+(use-package sudo-edit
+  :ensure t
+  :bind ("C-c s" . sudo-edit))
+)
+
+(use-package rainbow-delimiters
+  :ensure t
+  :init
+  (rainbow-delimiters-mode 1))
+
+(setq ido-enable-flex-matching nil)
+(setq ido-create-new-buffer 'always)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+(use-package ido-vertical-mode
+  :ensure t
+  :init
+  (ido-vertical-mode 1))
+(setq ido-vertical-define-keys 'C-n-and-C-p-only)
+
+(use-package smex
+  :ensure t
+  :init (smex-initialize)
+  :bind ("M-x" . smex))
+
+(setq ibuffer-expert t)
+
+(use-package doom-modeline
+  :ensure t
+  :hook (after-init . doom-modeline-mode))
+(use-package all-the-icons
+  :ensure t)
+
+(use-package popup-kill-ring
+  :ensure t
+  :bind ("M-y" . 'popup-kill-ring))
 
 (global-set-key (kbd "<s-return>") 'ansi-term)
 
@@ -46,6 +96,60 @@
   (split-window-right)
   (balance-windows)
   (other-window 1))
+
+(if (eq system-type 'windows-nt)
+    (defun toggle-full-screen () (interactive) (shell-command "emacs_fullscreen.exe"))
+    (global-set-key [f11] 'toggle-full-screen)
+)
+
+(defun kill-current-buffer ()
+  (interactive)
+  (kill-buffer (current-buffer)))
+(global-set-key (kbd "C-x k") 'kill-current-buffer)
+
+(defun kill-all-buffers ()
+  (interactive)
+  (mapc 'kill-buffer (buffer-list)))
+(global-set-key (kbd "C-c k a") 'kill-all-buffers)
+
+(defun copy-whole-line ()
+  (interactive)
+  (save-excursion
+    (kill-new
+     (buffer-substring
+      (point-at-bol)
+      (point-at-eol)))))
+(global-set-key (kbd "C-c y y") 'copy-whole-line)
+
+(defun kill-whole-word ()
+  (interactive)
+  (backward-word)
+  (kill-word 1))
+(global-set-key (kbd "C-c i w") 'kill-whole-word)
+
+(defun kill-current-line (&optional n)
+  (interactive "p")
+  (save-excursion
+    (beginning-of-line)
+    (let ((kill-whole-line t))
+      (kill-line n))))
+(global-set-key (kbd "C-c d d") 'kill-current-line)
+
+(defun new-line-above ()
+  (interactive)
+  (unless (bolp)
+    (beginning-of-line))
+  (newline)
+  (forward-line -1)
+  (indent-according-to-mode))
+(global-set-key (kbd "C-c o") 'new-line-above)
+
+(defun new-line-below ()
+  (interactive)
+  (unless (eolp)
+    (end-of-line))
+  (newline-and-indent))
+(global-set-key (kbd "C-c O") 'new-line-below)
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
@@ -85,25 +189,15 @@
   (interactive (list TERM_SHELL)))
 (ad-activate 'ansi-term)
 
+(line-number-mode 1)
+(column-number-mode 1)
+
 (setq make-backup-file nil)
 (setq auto-save-default nil)
 
-(setq ido-enable-flex-matching nil)
-(setq ido-create-new-buffer 'always)
-(setq ido-everywhere t)
-(ido-mode 1)
+;; Currently disabled
+;; Open temporary buffer in same window'C'
+; (setq org-src-window-setup 'current-window)
 
-(use-package ido-vertical-mode
-  :ensure t
-  :init
-  (ido-vertical-mode 1))
-(setq ido-vertical-define-keys 'C-n-and-C-p-only)
-
-(use-package smex
-  :ensure t
-  :init (smex-initialize)
-  :bind ("M-x" . smex))
-
-(setq ibuffer-expert t)
-
-(setq org-src-window-setup 'current-window)
+(add-to-list 'org-structure-template-alist
+	     '("el" "#+BEGIN_SRC emacs-lisp\n?\n#+END_SRC"))
--- init.el
+++ init.el
@@ -20,9 +20,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(package-selected-packages
    (quote
-    (smex ido-vertical-mode avy org-bullets which-key monokai-pro-theme use-package))))
+    (spacemacs-theme company popup-kill-ring doom-modeline monokai-pro-classic-theme rainbow-delimiters sudo-edit hungry-delete smex ido-vertical-mode avy org-bullets which-key monokai-pro-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
